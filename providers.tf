8376862e5911074687566572001f7973f85f0f0f
Mbali Matlakane <mbalim@restack.co.za>
2023-11-07 12:39:05 +0200

    remove yml files

--- providers.tf
+++ providers.tf
@@ -2,11 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.22.0"
+      #version = "5.22.0"
     }
   }
 }
 provider "aws" {
   region  = "us-east-1"
   #profile = "default"
+  export AWS_ACCESS_KEY_ID="anaccesskey"
+  export AWS_SECRET_ACCESS_KEY="asecretkey"
 }
